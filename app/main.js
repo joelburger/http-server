c4bad4eb54d692f9d4dd470945f7cce46b710964
joel <joel@burgerjoe.dev>
2024-09-21 08:43:09 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,13 @@
 const net = require("net");
 
+const echoMatcher = /^\/echo\/(?<echoValue>.+)$/;
+
+function extractParameter(pattern, parameter, path) {
+    const matches = pattern.exec(path);
+
+    return matches.groups[parameter];
+}
+
 function parseRequest(data) {
 
     // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
@@ -18,14 +26,15 @@ function parseRequest(data) {
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-
         const {verb, path} = parseRequest(data);
 
         if (path === '/') {
             socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        } if (path.startsWith('/echo')) {
+            const echoValue = extractParameter(echoMatcher, 'echoValue', path);
+            socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoValue.length}\r\n\r\n${echoValue}`));
         } else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-
+            socket.write(Buffer.from('HTTP/1.1 404 Not Found\r\n\r\n'));
         }
     });
 
