6aff768060956f3851dc272e3c6d879dab3268da
joel <joel@burgerjoe.dev>
2024-09-20 21:52:24 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,9 +1,16 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.on('data', (data) => {
+
+        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    });
+
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
+
+
