7df275d88c08ebb298ae71da92288c1b31562267
joel <joel@burgerjoe.dev>
2024-09-21 16:59:33 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,48 +1,77 @@
-const net = require("net");
+const net = require('net');
 
-const echoMatcher = /^\/echo\/(?<echoValue>.+)$/;
+const HttpCodes = {
+  Ok: {
+    code: 200,
+    text: 'OK',
+  },
+  NotFound: {
+    code: 404,
+    text: 'Not Found',
+  },
+};
+
+const ContentTypes = {
+  TextPlain: {
+    text: 'text/plain',
+  },
+};
 
 function extractParameter(pattern, parameter, path) {
-    const matches = pattern.exec(path);
+  const matches = pattern.exec(path);
 
-    return matches.groups[parameter];
+  return matches.groups[parameter];
 }
 
 function parseRequest(data) {
+  const stringValue = data.toString();
 
-    // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
+  const requestLine = stringValue.substring(0, stringValue.indexOf('\r\n'));
+  const [verb, path, version] = requestLine.split(' ');
 
-    const stringValue = data.toString();
+  const headerLines = stringValue.substring(requestLine.length, stringValue.indexOf('\r\n\r\n'));
 
-    const requestLine = stringValue.substring(0, stringValue.indexOf('\r\n'));
-    const [verb, path, version] = requestLine.split(' ');
+  const headers = headerLines.split('\r\n').reduce((acc, currentValue) => {
+    const [headerKey, headerValue] = currentValue.split(':').map((value) => value.trim());
+    return acc.set(headerKey, headerValue);
+  }, new Map());
 
-    return {
-        verb,
-        path,
-    };
+  return {
+    verb,
+    path,
+    headers,
+  };
 }
 
+function constructResponse(httpCode, contentType, content) {
+  if (content && contentType) {
+    return `HTTP/1.1 ${httpCode.code} ${httpCode.text}\r\nContent-Type: ${contentType.text}\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+  }
+  return `HTTP/1.1 ${httpCode.code} ${httpCode.text}\r\n\r\n`;
+}
+
+function handleRequest(socket, data) {
+  const { verb, path, headers } = parseRequest(data);
+
+  if (path === '/') {
+    socket.write(Buffer.from(constructResponse(HttpCodes.Ok)));
+  } else if (path.startsWith('/echo')) {
+    const echoValue = extractParameter(/^\/echo\/(?<echoValue>.+)$/, 'echoValue', path);
+    socket.write(Buffer.from(constructResponse(HttpCodes.Ok, ContentTypes.TextPlain, echoValue)));
+  } else if (path === '/user-agent') {
+    const userAgent = headers.get('User-Agent');
+    socket.write(Buffer.from(constructResponse(HttpCodes.Ok, ContentTypes.TextPlain, userAgent)));
+  } else {
+    socket.write(Buffer.from(constructResponse(HttpCodes.NotFound)));
+  }
+}
 
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        const {verb, path} = parseRequest(data);
+  socket.on('data', (data) => handleRequest(socket, data));
 
-        if (path === '/') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n');
-        } if (path.startsWith('/echo')) {
-            const echoValue = extractParameter(echoMatcher, 'echoValue', path);
-            socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoValue.length}\r\n\r\n${echoValue}`));
-        } else {
-            socket.write(Buffer.from('HTTP/1.1 404 Not Found\r\n\r\n'));
-        }
-    });
-
-    socket.on("close", () => {
-        socket.end();
-    });
+  socket.on('close', () => {
+    socket.end();
+  });
 });
 
-server.listen(4221, "localhost");
-
-
+server.listen(4221, 'localhost');
